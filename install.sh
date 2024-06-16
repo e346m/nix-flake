303f4c183af5cb304558e76f5bd46ff59ad82d4d
えいじ <eiji346g@gmail.com>
2024-06-16 08:05:16 +0000

    fix sh

--- install.sh
+++ install.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 # Move to the same directory as this script
 set -e
-FEATURE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${FEATURE_DIR}"
-
-# Option defaults
-VERSION="${VERSION:-"latest"}"
-MULTIUSER="${MULTIUSER:-"true"}"
-PACKAGES="${PACKAGES//,/ }"
-USEATTRIBUTEPATH="${USEATTRIBUTEPATH:-"false"}"
-FLAKEURI="${FLAKEURI:-""}"
-EXTRANIXCONFIG="${EXTRANIXCONFIG:-""}"
-USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
@@ -21,7 +10,6 @@ fi
 # Import common utils
 . ./utils.sh
 
-detect_user USERNAME
 
 if [ -e "/nix" ]; then
     echo "(!) Nix is already installed! Skipping installation."
@@ -38,5 +26,4 @@ else
         --extra-conf "sandbox = false" \
         --init none \
         --no-confirm
-    ENV PATH="${PATH}:/nix/var/nix/profiles/default/bin"
 fi
